76472a5011819158034578ffaaf1588037a5865d
Amit Gulati <amit@movenbank.com>
2012-01-23 20:42:56 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Musicmap::Application.config.secret_token = 'ae60383df2979ad784dd21d04915d94232d52088a67390d0510768d85d9f64e630014e585c1b596131dac78098a65d6ff8a50f8e50c9d0b00ffae542845fde1b'
